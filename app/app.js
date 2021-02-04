372e1393eb8bdd0f91e7a6d7f5c1fd464c12b304
Alireza <alirezahusseini33@gmail.com>
2021-02-04 22:54:23 +0300

    'animate-css-added'

---
+++ app/app.js
@@ -0,0 +1,32 @@
+const header = document.querySelector("header");
+const galleryImgBox = document.querySelectorAll(".img-box");
+const galleryImg = document.querySelectorAll(".g-img");
+
+// onscroll
+window.onscroll = function() {
+    let topPosition = window.scrollY;
+    if (topPosition >= 100) {
+        header.classList.add("header-onscroll");
+        // contactBtn.classList.add("carry");
+        // skillBtn.classList.add("carry-2");
+    } else {
+        header.classList.remove("header-onscroll");
+        // contactBtn.classList.remove("carry");
+        // skillBtn.classList.remove("carry-2");
+    }
+};
+
+// gallery img effect on hover
+$(function() {
+    var effects = "animate__animated animate__flipInX";
+    var effectEnd =
+        "animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd";
+
+    $(galleryImg).hover(function() {
+        $(this)
+            .addClass(effects)
+            .one(effectEnd, function() {
+                $(this).removeClass(effects);
+            });
+    });
+});
